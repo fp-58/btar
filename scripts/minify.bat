4dcfe354749b28df5e50739bb2c445f562397b4e
fp-58 <92231137+fp-58@users.noreply.github.com>
2023-12-21 22:56:47 -0500

    Fix paths in minify scripts and add comments

--- scripts/minify.bat
+++ scripts/minify.bat
@@ -1,1 +1,4 @@
+@REM Run this script from the root of the repository
+@REM Example: .\scripts\minify.bat
+
 @call npx terser index.js --config-file terser.json -o index.min.js
--- scripts/minify.sh
+++ scripts/minify.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+
+# Run this script from the root of the repository
+# Example: ./scripts/minify.bat
+
 npx terser index.js --config-file terser.json -o index.min.js
