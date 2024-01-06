188e4f4f3888624ef038863cf8fb628f4fb4c185
fp-58 <92231137+fp-58@users.noreply.github.com>
2024-01-06 18:04:48 -0500

    Add dev script to compile esmodule.js using index.ts

---
+++ scripts/compile.bat
@@ -0,0 +1,12 @@
+@REM Run this script from the root of the repository
+@REM Example: .\scripts\compile.sh
+
+
+@REM Generate index.js and definitions without comments
+@call npx tsc --removeComments
+
+@REM Generate definition files with comments
+@call npx tsc --noEmit
+
+@REM Rename compiled files (for backwards compatibility with v1.0.0)
+@move lib/index.js lib/esmodule.js
---
+++ scripts/compile.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Run this script from the root of the repository
+# Example: ./scripts/compile.sh
+
+
+# Generate index.js and definitions without comments
+npx tsc --removeComments
+
+# Generate definition files with comments
+npx tsc --noEmit
+
+# Rename compiled files (for backwards compatibility with v1.0.0)
+mv lib/index.js lib/esmodule.js
