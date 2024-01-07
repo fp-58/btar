281076c8f16d05b06a8cfc9c3a457e58eca7c092
fp-58 <92231137+fp-58@users.noreply.github.com>
2024-01-06 20:35:49 -0500

    Add sourcemaps for debugging

--- scripts/compile.bat
+++ scripts/compile.bat
@@ -1,15 +1,15 @@
 @REM Run this script from the root of the repository
-@REM Example: .\scripts\compile.sh
+@REM Example: .\scripts\compile.bat
 
 
-@REM Generate index.js and definitions without comments
+@REM Generate esmodule.js and definitions without comments
 @call npx tsc --removeComments
 
 @REM Generate definition files with comments
 @call npx tsc --emitDeclarationOnly
 
-@REM Rename compiled files (for backwards compatibility with v1.0.0)
-@move lib\index.js lib\esmodule.js
+@REM Move generated definitions
+@move types\esmodule.d.ts types\index.d.ts
 
 @REM Generate index.js using esmodule.js with rollup
-@call npx rollup lib\esmodule.js -o lib\index.js -f umd -n btar
+@call npx rollup lib\esmodule.js -o lib\index.js -f umd -n btar -m --sourcemapExcludeSources
--- scripts/compile.sh
+++ scripts/compile.sh
@@ -4,14 +4,14 @@
 # Example: ./scripts/compile.sh
 
 
-# Generate index.js and definitions without comments
+# Generate esmodule.js and definitions without comments
 npx tsc --removeComments
 
 # Generate definition files with comments
 npx tsc --emitDeclarationOnly
 
-# Rename compiled files (for backwards compatibility with v1.0.0)
-mv lib/index.js lib/esmodule.js
+# Move generated definitions
+mv types/esmodule.d.ts types/index.d.ts
 
 # Generate index.js using esmodule.js with rollup
-npx rollup lib/esmodule.js -o lib/index.js -f umd -n btar
+npx rollup lib/esmodule.js -o lib/index.js -f umd -n btar -m --sourcemapExcludeSources
